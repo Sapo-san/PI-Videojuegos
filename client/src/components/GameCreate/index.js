57a529402543cabe68b6e6405a5b81b3ce8985f8
Sapo-san <cristobal.herreros@gmail.com>
2022-05-16 23:07:18 -0400

    terminada validacion del form controlado, falta POSTear el form y la ruta

--- client/src/components/GameCreate/index.js
+++ client/src/components/GameCreate/index.js
@@ -16,6 +16,16 @@ const GameCreate = () => {
     background_image: ""
   })
 
+  const [formErrors, setFormErrors] = useState({
+    name: null,
+    description: null,
+    genres: null,
+    release_date: null,
+    rating: null,
+    platforms: null,
+    background_image: null
+  })
+
   const [availableGenres, setAvailableGenres] = useState(null)
   const [selectedGenre, setSelectedGenre] = useState(['',0]) // el cero es variable de control
 
@@ -62,6 +72,101 @@ const GameCreate = () => {
     })
   }
 
+  const validateForm = () => {
+    console.log("state:",formState)
+    console.log("previous err:",formErrors)
+
+    let foundErrors = {
+      ...formErrors
+    }
+  
+    if (!formState.name) {
+      foundErrors.name = "El juego debe tener un nombre"
+    } else {
+      foundErrors.name = null
+    }
+
+    if (formState.name && formState.length > 70 ) {
+      foundErrors.name = "El nombre del juego no debe tener más de 70 caracteres"
+    } 
+
+    if (formState.genres.length === 0) {
+      foundErrors.genres = "El juego debe tener al menos un género"
+    } else {
+      foundErrors.genres = null
+    }
+
+    if (!formState.description) {
+      foundErrors.description = "El juego debe tener una descripción"
+    } else {
+      foundErrors.description = null
+    }
+
+    if (!formState.release_date) {
+      foundErrors.release_date = "El juego debe tener una fecha de lanzamiento"
+    } else {
+      foundErrors.release_date = null
+    }
+
+    if (!formState.platforms) {
+      foundErrors.platforms = "El juego debe tener al menos una plataforma"
+    } else {
+      foundErrors.platforms = null
+    }
+
+    if (formState.background_image.length > 255 ) {
+      foundErrors.genres = "El link a la imagen del juego no puede tener más de 255 carácteres"
+    } else {
+      foundErrors.background_image = null
+    }
+
+    if (!foundErrors) {
+      //hacer post owo
+      console.log("No hay errores wiii")
+    } else {
+      setFormErrors(foundErrors)
+      console.log("current err:",foundErrors)
+    }
+  } 
+
+  const clearValidation = (whichValidation) => {
+    switch (whichValidation) {
+      case 'name':
+        setFormErrors({...formErrors, name: null})
+        return
+      case 'genres':
+        setFormErrors({...formErrors, genres: null})
+        return
+      case 'description':
+        setFormErrors({...formErrors, description: null})
+        return
+      case 'release_date':
+        setFormErrors({...formErrors, release_date: null})
+        return
+      case 'platforms':
+        setFormErrors({...formErrors, platforms: null})
+        return
+      case 'background_image':
+        setFormErrors({...formErrors, background_image: null})
+        return
+      
+      case 'all':
+        setFormErrors({
+          name: null,
+          description: null,
+          genres: null,
+          release_date: null,
+          rating: null,
+          platforms: null,
+          background_image: null
+        })
+        return
+
+      default:
+        break;
+    }
+  }
+
   /** Retorna html de la selección de generos del form */
   const genreSelector = () => {
 
@@ -74,7 +179,7 @@ const GameCreate = () => {
       return (<p>Géneros: Cargando...</p>)
     } else {
       return (<>
-      <p>Géneros:</p>
+      <p>Géneros: <i className="warning">{formErrors.genres}</i></p>
       <div className='displayGenreSelectorStuff'>
         <select value={selectedGenre[0]} onChange={(e) => setSelectedGenre([e.target.value,selectedGenre[1]])}>
           {listAvaibleGenres()}
@@ -84,7 +189,9 @@ const GameCreate = () => {
           if (!formState.genres.includes(parseInt(selectedGenre))) {
             // si NO incluye el genero seleccionado (evitar que se repitan generos en el form)
             setFormState({...formState, genres: [...formState.genres, parseInt(selectedGenre)]})
+            clearValidation('genres')
           }
+          
         }}>Añadir Género</button>
       </div>
         
@@ -96,6 +203,8 @@ const GameCreate = () => {
       </>)
     }
   }
+
+
 
   return (
     <div className='gameCreateContainer'>
@@ -111,63 +220,85 @@ const GameCreate = () => {
 
         <form className='gameCreateform'>
           <div className='inputContainer'>
-              <p>Nombre:</p>
+              <p>Nombre: <i className='warning'>{formErrors.name}</i></p>
               <input
                 type="text"
                 name="name"
                 placeholder='Nombre del juego...'
-                onChange={(event) => {setFormState({...formState, name: event.target.value})}}>
+                onChange={(event) => {
+                  setFormState({...formState, name: event.target.value})
+                  clearValidation('name')
+                  }}>
               </input>
           </div>
           <div className='inputContainer'>
               {genreSelector()}
           </div>
           <div className='inputContainer'>
-            <p>Descripción:</p>
+            <p>Descripción: <i className='warning'>{formErrors.description}</i></p>
             <textarea
               name="description"
               rows="10"
               placeholder='Ingresa la descripción aquí...'
-              onChange={(event) => {setFormState({...formState, description: event.target.value})}}>
+              onChange={(event) => {
+                setFormState({...formState, description: event.target.value})
+                clearValidation('description')
+                }}>
             </textarea>
           </div>
           <div className='inputContainer'>
-            <p>Fecha de lanzamiento: {formState.release_date}</p>
+            <p>Fecha de lanzamiento: {formState.release_date} <i className='warning'>{formErrors.release_date}</i></p>
             <input
               type="date"
               name="release_date"
-              onChange={(event) => {setFormState({...formState, release_date: event.target.value})}}>
+              onChange={(event) => {
+                setFormState({...formState, release_date: event.target.value})
+                clearValidation('release_date')
+                }}>
             </input>
           </div>
           <div className='inputContainer'>
-            <p>Rating: {formState.rating}</p>
+            <p>Rating: {formState.rating} </p>
             <input
               type="range"
               name="rating"
               min="0" max="5"
               step="0.01"
               value={formState.rating}
-              onChange={(event) => {setFormState({...formState, rating: event.target.value})}}>
+              onChange={(event) => {
+                setFormState({...formState, rating: event.target.value})
+                //validateForm(event,'rating')
+                }}>
             </input>
           </div>
           <div className='inputContainer'>
-            <p>Plataformas: (ingresar separadas por espacios)</p>
+            <p>Plataformas: (ingresar separadas por espacios) <i className='warning'>{formErrors.platforms}</i></p>
             <input
               type="text"
               name="platforms"
               placeholder='ejemplo: pc xbox-one playstation-4 nintendo-switch'
-              onChange={(event) => {setFormState({...formState, platforms: event.target.value})}}>
+              onChange={(event) => {
+                setFormState({...formState, platforms: event.target.value})
+                clearValidation('platforms')
+                }}>
             </input>
           </div>
           <div className='inputContainer'>
-            <p>Link a la imagen del juego</p>
+            <p>Link a la imagen del juego: <i className='warning'>{formErrors.background_image}</i></p>
             <input
               type="text"
               name="background_image"
-              onChange={(event) => {setFormState({...formState, background_image: event.target.value})}} placeholder='http://link-a/una/imagen.png'>
+              onChange={(event) => {
+                setFormState({...formState, background_image: event.target.value})
+                clearValidation('description')
+                }} placeholder='http://link-a/una/imagen.png'>
             </input>
           </div>
-          <button className="backLink"> Crear Juego</button>
+          <button onClick={(e) => {
+            e.preventDefault()
+            clearValidation('all')
+            validateForm()
+            }} className="backLink"> Crear Juego</button>
         </form>
       </div>
 
